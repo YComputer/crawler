3f6b15cf8941e547c498393e26557f7067673805
YComputer <thea@163.com>
2016-04-28 16:11:10 +0800

    get data

---
+++ vc_contact_crawler2.js
@@ -0,0 +1,68 @@
+/*
+数据来自于
+http://startupboard.sudoboot.com/contacts
+联系人数据格式
+{
+    name:'陈新',
+    company:'红杉资本',
+    position:'早中期TMT',
+    email:'cchen@sequoiacap.com'
+}
+*/
+
+var http = require('http')
+var cheerio = require('cheerio')
+var fs = require('fs')
+var page = 1
+
+var baseUrl = 'http://startupboard.sudoboot.com/contacts?page='
+for(var pageNumber = 1; pageNumber<75; pageNumber++){
+    console.log(pageNumber)
+    http.get(baseUrl+pageNumber, function(res) {
+    console.log(baseUrl+pageNumber + '----------')
+    var html = ''
+    res.on('data', function(data) {
+        html += data
+    })
+
+    res.on('end', function() {
+        filterContacts(html, pageNumber)
+    })
+}).on('error', function(error) {
+    console.log('获取数据出错')
+    console.log(error)
+})
+}
+
+
+function filterContacts(html, pageNumber) {
+    var contacts = []
+    var $ = cheerio.load(html)
+    var contacts = $('tbody').children()
+    contacts.each(function(item) {
+        // 这里为什么console.log($(item).find('td').length) 的值是0，为什么？？
+        // console.log($(this).find('td').length)的值为4，这是想要的结果。
+        var details = $(this).find('td')
+        var singleContact = {}
+        details.each(function(item) {
+            //console.log('---',$(this).attr('class'))
+            //console.log('---',$(this).text().trim())
+            var type = $(this).attr('class').toString().trim()
+            var value = $(this).text().toString().trim()
+            if (type == 'name') {
+                singleContact.name = value
+            } else if (type == 'company') {
+                singleContact.company = value
+            } else if (type == 'position') {
+                singleContact.position = value
+            } else if (type == 'email') {
+                singleContact.email = value
+            } else {
+                console.log('data parse error')
+            }
+        })
+
+        fs.appendFile('contact'+pageNumber+'.txt', JSON.stringify(singleContact) + '\n')
+            // console.log(singleContact)
+    })
+}
